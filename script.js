46780e15e930b5182822da64d3b76f63e1aeddb8
dzulfilove <dzulfivector@gmail.com>
2023-04-05 06:39:58 -0700

    edit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountains_behind = document.getElementById('mountains_behind');
 let mountains_front = document.getElementById('mountains_front');
+let front = document.getElementById('front');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 
@@ -12,6 +13,7 @@ window.addEventListener('scroll', function(){
     stars.style.left = value * 0.25 + 'px';
     moon.style.top = value * 1.05 + 'px';
     mountains_front.style.top = value * 0 + 'px';
+    front.style.top = value * 0 + 'px';
     text.style.marginRight = value * 7 + 'px';
     text.style.marginTop = value * 1.5 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
